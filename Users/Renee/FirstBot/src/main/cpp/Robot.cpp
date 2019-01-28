26872be664e3efae505d041a7f37291256d5cfb4
marc451 <marcjswanson@hotmail.com>
2019-01-28 06:16:23 -0600

    Ultrasonic equation added

--- Users/Renee/FirstBot/src/main/cpp/Robot.cpp
+++ Users/Renee/FirstBot/src/main/cpp/Robot.cpp
@@ -1,21 +1,75 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
 #include "Robot.h"
+#include <iostream>
 
-void Robot::RobotInit() {}
 
-void Robot::AutonomousInit() {}
-void Robot::AutonomousPeriodic() {}
+void Robot::RobotInit() {
+  
+}
 
-void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {}
 
-void Robot::TestInit() {}
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {
+  std::cout << "I'm in AutonomouseInit()" << std::endl;
+}
+
+void Robot::AutonomousPeriodic() {
+
+  frameCounter = frameCounter + 1;
+  
+  if (frameCounter == 50) {
+    std::cout << countCounter << std::endl;
+    countCounter = countCounter + 1;
+    frameCounter = 0;
+  }
+}
+
+void Robot::TeleopInit() {
+  
+  std::cout << "TeleopInit, READY!!" << std::endl;
+}
+
+void Robot::TeleopPeriodic() {
+
+  /*
+  if ( m_gamepad.GetBumper(frc::GenericHID::kRightHand)) {
+    std::cout << "Right Button Pressed!" << std::endl;
+  }
+  if ( m_gamepad.GetBumper(frc::GenericHID::kLeftHand)) {
+    std::cout << "Left Button Pressed!" << std::endl;
+  }
+  */
+
+/*
+  if ( m_gamepad.GetBumper(frc::GenericHID::kRightHand)) {
+    
+    m_gamepad.SetRumble(frc::GenericHID::RumbleType::kRightRumble, 1.0);
+    m_gamepad.SetRumble(frc::GenericHID::RumbleType::kLeftRumble, 0.0);
+  } 
+
+  if ( m_gamepad.GetBumper(frc::GenericHID::kLeftHand)) {
+    m_gamepad.SetRumble(frc::GenericHID::RumbleType::kLeftRumble, 1.0);
+    m_gamepad.SetRumble(frc::GenericHID::RumbleType::kRightRumble, 0.0);
+  }
+
+  if (m_gamepad.GetAButton()){
+    m_gamepad.SetRumble(frc::GenericHID::RumbleType::kLeftRumble, 0.0);
+    m_gamepad.SetRumble(frc::GenericHID::RumbleType::kRightRumble, 0.0);
+  }
+*/
+  double currentDistance = m_ultrasonic.GetValue() * 0.140-1.32;
+  std::cout << currentDistance << std::endl;
+
+
+}
+
 void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
--- Users/Renee/FirstBot/src/main/include/Robot.h
+++ Users/Renee/FirstBot/src/main/include/Robot.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,18 +7,31 @@
 
 #pragma once
 
+#include <string>
+#include <XboxController.h>
 #include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <Ultrasonic.h>
+#include <frc/AnalogInput.h>
 
 class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override;
-
+  void RobotPeriodic() override;
   void AutonomousInit() override;
   void AutonomousPeriodic() override;
-
   void TeleopInit() override;
   void TeleopPeriodic() override;
-
-  void TestInit() override;
   void TestPeriodic() override;
+
+ private:
+  	frc::XboxController m_gamepad{0}; 
+
+    int frameCounter = 0;
+    int countCounter = 1;
+
+    frc::AnalogInput m_ultrasonic{2};
+   
+    static constexpr double kValueToInches = 0.125;
+
 };
